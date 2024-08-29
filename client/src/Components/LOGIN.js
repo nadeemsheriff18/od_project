a45205193a6db90725780aaebd7916f29df53c8b
kabelan-10 <ekabelan28@gmail.com>
2024-08-29 14:47:32 +0530

    LOGIN page (with header)

---
+++ client/src/Components/LOGIN.js
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import OdLoginPage from "./OdLoginPage";
+import Odheader from "./Odheader";
+function LOGIN() {
+  return (
+    <div className="relative">
+      <Odheader />
+      <OdLoginPage />
+    </div>
+  );
+}
+
+export default LOGIN;
---
+++ client/src/Components/Odheader.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Odheader() {
+  return (
+    <div className="flex justify-between bg-violet-100 drop-shadow-xl min-h-24 pl-40 pr-40 pt-5 absolute w-full">
+      <img src="" alt="REC" />
+      <img src="" alt="RI" />
+    </div>
+  );
+}
+
+export default Odheader;
