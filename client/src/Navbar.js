411d28bb9a98f75fd2690cf812f82ee90480217a
Nadeem Sheriff A <137400851+nadeemsheriff18@users.noreply.github.com>
2024-08-29 13:59:04 +0530

    second

---
+++ client/src/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='flex justify-between bg-[#f6d7fc]'>
+        <img src='./assets/rec.png' alt="rec logo" className='w-1/6 h-1/6'></img>
+        <h3 className='font-bold text-2xl font-sans text-[#a247b3]'>REC</h3>
+    </div>
+  )
+}
+
+export default Navbar
