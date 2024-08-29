122c10367671606225a599ace7eb0bf4552f056d
kabelan-10 <ekabelan28@gmail.com>
2024-08-29 14:38:46 +0530

    RECCCC

--- client/src/Navbar.js
+++ client/src/Navbar.js
@@ -4,7 +4,7 @@ const Navbar = () => {
   return (
     <div className='flex justify-between bg-[#f6d7fc]'>
         <img src='./assets/rec.png' alt="rec logo" className='w-1/6 h-1/6'></img>
-        <h3 className='font-bold text-2xl font-sans text-[#a247b3]'>REC</h3>
+        <h3 className='font-bold text-2xl font-sans text-[#a247b3]'>RECCCC</h3>
     </div>
   )
 }
