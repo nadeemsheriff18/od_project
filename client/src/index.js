280f4bc3dbf0ab74ac7c5244f24ad085f0f09289
kabelan-10 <ekabelan28@gmail.com>
2024-08-29 14:13:50 +0530

    kabi

--- client/src/index.js
+++ client/src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import Student from './Student';
+import LOGIN from "./Components/LOGIN.js";
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Student />
+    <LOGIN/>
+    {/* <Student /> */}
   </React.StrictMode>
 );
 
