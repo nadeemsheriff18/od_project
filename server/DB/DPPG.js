1f726f764c4a0aca9597c69c1a48a903202d0231
Nadeem Sheriff A <137400851+nadeemsheriff18@users.noreply.github.com>
2024-10-13 12:21:56 +0530

    Update DPPG.js

--- server/DB/DPPG.js
+++ server/DB/DPPG.js
@@ -4,7 +4,7 @@ import cron from 'node-cron';
 
 //!!!!!!!!! Unthula work pandra apo . entha comment out paniko !!!!!!!!!!!!!!!!!!!!!!!!
 //KABI
-
+{/*}
 const pool = new Pool({
     user: "postgres",
     host: "localhost",
@@ -13,15 +13,15 @@ const pool = new Pool({
     port: 5432,          
     
 });
-
+*/}
 //Nadeem
-//  const pool = new Pool({
-//          user: 'postgres',
-//          password: '160427',
-//            host: 'localhost',
-//            port: 5432,
-//           database: 'OD'
-//        });
+  const pool = new Pool({
+         user: 'postgres',
+         password: '160427',
+           host: 'localhost',
+           port: 5432,
+          database: 'OD'
+       });
 
 // Export the pool instance
 // const updateExpiredRecords = async () => {
